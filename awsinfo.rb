98561ca9640ceee124226dad1a5ea286bc141e33
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2018-05-28 11:46:04 -0400

    Update awsinfo.rb

--- awsinfo.rb
+++ awsinfo.rb
@@ -1,9 +1,9 @@
 class Awsinfo < Formula
   desc "AWS CLI Information Utility"
   homepage "https://github.com/lencap/awsinfo/"
-  url "https://github.com/lencap/awsinfo/releases/download/2.0.9/awsinfo"
-  sha256 "cd7eb2a0d84b99d7ebabee4f91ad1d56b7609610bae7b84d1fe15690a15a5301"
-  version "2.0.9"
+  url "https://github.com/lencap/awsinfo/releases/download/2.0.10/awsinfo"
+  sha256 "8cbddc55f83fcac99cca63a0f612cb28de9577fbcd59da4910203d57c042f3d2"
+  version "2.0.10"
   
   bottle :unneeded
   
