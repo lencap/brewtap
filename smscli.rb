e922a87acc7e67dac8ca9ef315921b2a7d098e37
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2019-12-29 13:56:05 -0500

    Update smscli.rb

--- smscli.rb
+++ smscli.rb
@@ -2,7 +2,7 @@ class Smscli < Formula
   desc "SMS CLI utility"
   homepage "https://github.com/lencap/smscli"
   version "1.0.4"
-  url "https://github.com/lencap/smscli/releases/download/v1.0.4/smscli-1.0.4.tar.gz"
+  url "https://github.com/lencap/smscli/releases/download/v1.0.4/smscli-1.0.4.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "0ad9b3e98a51063ea3825ecd5836fdc9b80be8ff382c1091218e24eb33b6d429"
 
   bottle :unneeded
