71efbb39731b9c8813825a17a7ce6270d76286cb
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2019-12-29 00:31:36 -0500

    Create smsclic.rb

---
+++ smsclic.rb
@@ -0,0 +1,17 @@
+class Smsclic < Formula
+  desc "SMS CLI utility"
+  homepage "https://github.com/lencap/smsclic"
+  version "1.0.0"
+  url "https://github.com/lencap/smsclic/releases/download/v1.0.0/smsclic-1.0.0.tar.gz"
+  sha256 "dcc22956d5e62b8079d5eb1ee6ee7e20d5f57fd9956824478df3c485301913f3"
+
+  bottle: unneeded
+    
+  def install
+    bin.install "smsclic"
+  end
+
+  test do
+    system "#{bin}/smsclic"
+  end
+end
