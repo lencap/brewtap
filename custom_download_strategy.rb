877911740dbc0eceb36757e73f72782ce047db12
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2019-12-29 16:11:05 -0500

    Update custom_download_strategy.rb

--- custom_download_strategy.rb
+++ custom_download_strategy.rb
@@ -37,7 +37,7 @@ class CustomGitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 
   def set_github_token
-    @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN2"]
+    @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"]
     unless @github_token
       raise CurlDownloadStrategyError, "Environmental variable HOMEBREW_GITHUB_API_TOKEN is required."
     end
