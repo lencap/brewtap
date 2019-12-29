d3582cfc049091af3044f2d8e342d4b21c29fa11
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2019-12-29 00:42:18 -0500

    Update smsclic.rb

--- smsclic.rb
+++ smsclic.rb
@@ -5,7 +5,7 @@ class Smsclic < Formula
   url "https://github.com/lencap/smsclic/releases/download/v1.0.0/smsclic-1.0.0.tar.gz"
   sha256 "dcc22956d5e62b8079d5eb1ee6ee7e20d5f57fd9956824478df3c485301913f3"
 
-  bottle: unneeded
+  bottle :unneeded
     
   def install
     bin.install "smsclic"
