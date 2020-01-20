e9a576b119ee1925a3f486ad8f059591112b7ec7
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2020-01-20 15:56:26 -0500

    Update vm.rb

--- vm.rb
+++ vm.rb
@@ -1,9 +1,9 @@
 class Vm < Formula
   desc "Simple CentOS VM Manager"
   homepage "https://github.com/lencap/vm"
-  version "2.4.4"
-  url "https://github.com/lencap/vm/releases/download/v2.4.4/vm-2.4.4.tar.gz"
-  sha256 "1be0e1c6090ab0cd585953b7015a533a9ba6ce12630eb884e029922915b0d6ee"
+  version "2.5.0"
+  url "https://github.com/lencap/vm/releases/download/v2.5.0/vm-2.5.0.tar.gz"
+  sha256 "1d53bb8cbd4680bac7bf402e3d2b301ef211e5be9d836d7e398a03257ac2d4ad"
 
   bottle :unneeded
     
