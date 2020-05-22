7b220f922553eba17dee30e28bdb93d6e5d37a76
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2020-05-21 20:02:58 -0400

    Update vm.rb

--- vm.rb
+++ vm.rb
@@ -1,9 +1,9 @@
 class Vm < Formula
   desc "Simple Linux VM Manager"
   homepage "https://github.com/lencap/vm"
-  version "266"
-  url "https://github.com/lencap/vm/releases/download/v266/vm-266.tar.gz"
-  sha256 "a79bcc8766f59b1e94ee2a74997a45ed6d5d3cc94c7c9d60e8579af6a0e7e085"
+  version "267"
+  url "https://github.com/lencap/vm/releases/download/v267/vm-267.tar.gz"
+  sha256 "499f08e3371fd47dab8ff600e93ef38c37f36005ceddf3054738f2ceca5bf009"
 
   bottle :unneeded
     
